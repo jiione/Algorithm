178c06ad3900dd2b1a72ff8283ba6d116ba58d76
jiwon <83341978+jiione@users.noreply.github.com>
2022-12-21 22:40:12 +0900

    [Silver I] Title: 하노이 탑 이동 순서, Time: 124 ms, Memory: 2208 KB -BaekjoonHub

--- 백준/Silver/11729. 하노이 탑 이동 순서/하노이 탑 이동 순서.cc
+++ 백준/Silver/11729. 하노이 탑 이동 순서/하노이 탑 이동 순서.cc
@@ -1,27 +1,27 @@
-#include <stdio.h>
-#include <iostream>
-#include <cmath>
+#include<iostream>
+#include<algorithm>
+#include<vector>
+#include<cmath>
 
 using namespace std;
 
-
 void Hanoi(int n, int a, int b, int c) {
 	if (n == 1) {
-		printf("%d %d\n", a, c);
+		cout<<a<<" "<<c<<'\n';
 		return;
 	}
-	else {
-		Hanoi(n - 1, a, c, b);
-		printf("%d %d\n", a,c );
-		Hanoi(n - 1, b, a, c);
-	}
+    Hanoi(n - 1, a, c, b);
+    cout<<a<<" "<<c<<'\n';
+    Hanoi(n - 1, b, a, c);
 
 }
-
-int main() {
-	int N;
-	scanf("%d", &N);
-	printf("%d\n", (int)pow(2,N)-1);
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    int N;
+    cin>>N;
+    cout<< (int)pow(2,N)-1<<'\n';
 	Hanoi(N, 1, 2, 3);
 	
 	return 0;
