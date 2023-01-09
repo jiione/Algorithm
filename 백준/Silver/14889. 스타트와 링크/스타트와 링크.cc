40314cd37e5b898715775d59b53cb36960e7295f
jiwon <83341978+jiione@users.noreply.github.com>
2023-01-10 01:37:58 +0900

    Update 스타트와 링크.cc

--- 백준/Silver/14889. 스타트와 링크/스타트와 링크.cc
+++ 백준/Silver/14889. 스타트와 링크/스타트와 링크.cc
@@ -1,57 +1,59 @@
+#include<cmath>
 #include<iostream>
-#include<math.h>
+#include<algorithm>
+#include<vector>
+#include<stdlib.h>
+
 using namespace std;
 
-int stats[21][21];
+int board[21][21];
 bool check[22];
 int N;
-int ans = 1000000000;
+int ans = 10000000;
 
-void DFS(int x, int pos)
-{
-	if (x == N / 2)
-	{
-		int start, link;
-		start = 0;
-		link = 0;
+void dfs(int x, int cnt) {
+    if (x == N / 2) {
+        int start = 0;
+        int link = 0;
+        
+        for (int i = 1; i <= N; i++) {
+            for (int j = i + 1; j <= N; j++) {
+                if (check[i] == true && check[j] == true) {
+                    start += board[i][j];
+                    start += board[j][i];
+                }
+                else if (check[i] == false && check[j] == false) {
+                    link += board[i][j];
+                    link += board[j][i];
+                }
+            }
+        }
 
-		for (int i = 1; i <= N; i++)
-		{
-			for (int j = 1; j <= N; j++)
-			{
-				if (check[i] == true && check[j] == true) start += stats[i][j];
-				if (check[i] == false && check[j] == false) link += stats[i][j];
-			}
-		}
+        int tmp = abs(start - link);
+        if (ans > tmp)ans = tmp;
+        return;
+    }
 
-		int temp = abs(start - link);
-		if (ans > temp) ans = temp;
-
-		return;
-	}
-
-	for (int i = pos; i < N; i++)
-	{
-		check[i] = true;
-		DFS(x + 1, i + 1);
-		check[i] = false;
-	}
-
+    for (int i = cnt; i <= N; i++) {
+        check[i] = true;
+        dfs(x + 1, i + 1);
+        check[i] = false;
+    }
 }
 
-int main()
-{
-	cin >> N;
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    
+    cin >> N;
+   
+    for (int i = 1; i <= N; i++) {
+        for (int j = 1; j <= N; j++) {
+            cin >> board[i][j];
+        }
+    }
 
-	for (int i = 1; i <= N; i++)
-	{
-		for (int j = 1; j <= N; j++)
-		{
-			cin >> stats[i][j];
-		}
-	}
+    dfs(0, 1);
+    cout << ans;
 
-	DFS(0, 1);
-
-	cout << ans;
 }
