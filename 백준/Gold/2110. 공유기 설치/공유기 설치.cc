3479e9781ab8a4752ee1f97d0f2c771c18a97e37
jiwon <83341978+jiione@users.noreply.github.com>
2023-09-05 17:36:23 +0900

    [Gold IV] Title: 공유기 설치, Time: 32 ms, Memory: 3592 KB -BaekjoonHub

--- 백준/Gold/2110. 공유기 설치/공유기 설치.cc
+++ 백준/Gold/2110. 공유기 설치/공유기 설치.cc
@@ -1,47 +1,57 @@
-#include <iostream>
-#include <vector>
-#include <algorithm>
+#include<iostream>
+#include<vector>
+#include<algorithm>
 
 using namespace std;
 
-int main(void)
-{
-	int n, c, num, st, router, start, end, mid, ans = 0;
-	cin >> n >> c;
-	vector<int> pos;
-	for (int i = 0; i < n; i++)
-	{
-		cin >> num;
-		pos.push_back(num);
-	}
-	sort(pos.begin(), pos.end());
-	start = 1;                             // 최소 거리
-	end = pos[n - 1] - pos[0];             // 최대 거리
+#define endl '\n'
 
-	while (start <= end)
-	{
-		router = 1;
-		mid = (start + end) / 2;
-		st = pos[0];
+int N,C;
+int dist = 0;
+void binarySearch(vector<int> houses){
+    int start = 1;
+    int end = houses.back()-houses.front();
 
-		for (int i = 1; i < n; i++)
-		{
-			if (pos[i] - st >= mid)
-			{
-				router++;
-				st = pos[i];
-			}
-		}
+    while(end>=start){
+        int mid =(start + end)/2;
+        int cnt = 1;
+        int cur = houses.front();
+        for(int i=1; i<N; ++i){
+            if(houses[i]-cur >= mid){
+                cnt++;
+                cur = houses[i];
+            }
+            if(cnt>C) break;
+        }
 
-		if (router >= c)
-		{
-			ans = max(ans, mid);
-			start = mid + 1;
-		}
+        if(cnt<C){
+            end = mid -1;
+        }
+        else if(cnt>=C){
+            start = mid+1;
+            dist = max(mid,dist);
+        }
 
-		else
-			end = mid - 1;
-	}
-	cout << ans;
-	return 0;
+    }
+}
+
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    cin>>N>>C;
+
+    vector<int> houses(N,0);
+
+    for(int i=0; i<N; ++i){
+        cin>>houses[i];
+    }
+
+    sort(houses.begin(), houses.end());
+
+    binarySearch(houses);
+
+    cout<<dist<<endl;
+
+    return 0;
 }
