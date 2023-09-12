313ebaf8d514f436bd285e2f31e9a31d3b0af8d1
jiwon <83341978+jiione@users.noreply.github.com>
2023-09-12 21:42:56 +0900

    [Silver II] Title: 최대 힙, Time: 16 ms, Memory: 2916 KB -BaekjoonHub

---
+++ 백준/Silver/11279. 최대 힙/최대 힙.cc
@@ -0,0 +1,34 @@
+#include<iostream>
+#include<queue>
+
+#define endl '\n'
+
+using namespace std;
+
+priority_queue<int> pq;
+
+int N;
+
+int main(){
+	cin.tie(NULL);
+	ios::sync_with_stdio(false);
+
+	cin>>N;
+
+	for(int i=0; i<N; ++i){
+		int tmp;
+		cin>>tmp;
+		if(tmp!=0){
+			pq.push(tmp);
+		}
+		else if(pq.empty()){
+			cout<<0<<endl;
+		}
+		else{
+			cout<<pq.top()<<endl;
+			pq.pop();
+		}
+	}
+
+	return 0;
+}
