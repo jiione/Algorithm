7bacc81ac4e29bf287e93780d957700da8c6a22b
jiwon <83341978+jiione@users.noreply.github.com>
2023-11-07 17:22:38 +0900

    [Gold III] Title: 내리막 길, Time: 24 ms, Memory: 4228 KB -BaekjoonHub

--- 백준/Gold/1520. 내리막 길/내리막 길.cc
+++ 백준/Gold/1520. 내리막 길/내리막 길.cc
@@ -1,44 +1,39 @@
-#include <iostream>
+#include<iostream>
+#define endl '\n'
 using namespace std;
-int arr[501][501]= {};
-int dp[501][501] = {};
-bool visited[501][501] = { 0 };
-int m, n;
-int y_ar[4] = { 1, 0, -1, 0 };
-int x_ar[4] = { 0, 1, 0, -1 };
 
-int dfs(int cy, int cx) {
+int dx[4]={1,0,-1,0};
+int dy[4]={0,1,0,-1};
 
-	
-	if (cy == m  && cx == n) {
-		//dp[cy][cx] = 1;
-		return 1; //종점이라면 1을 반환합니다. 
-	}
-	if (visited[cy][cx]) //이미 방문한 곳이라면 해당 dp값을 전달해준다.
-		return dp[cy][cx];
-	visited[cy][cx] = 1;
-	for (int i = 0; i < 4; i++) {
-		int next_y = cy + y_ar[i];
-		int next_x = cx + x_ar[i];
+int map[501][501];
+int dp[501][501] = {0,};
+bool visited[501][501] ={false};
+int N,M;
 
-		if (next_y > 0 && next_y <= m && next_x > 0 && next_x <= n)
-			if (arr[cy][cx] > arr[next_y][next_x]) 
-				dp[cy][cx] += dfs(next_y, next_x); 
-		
-	}
-	return dp[cy][cx];
+int dfs(int x, int y){
+    if(x==M && y==N) return 1;
+    if(visited[y][x]) return dp[y][x];
+    visited[y][x] = true;
+    for(int i=0;i<4;++i){
+        int nx = x+dx[i];
+        int ny = y+dy[i];
+
+        if(nx > 0 && ny > 0 && nx <= M && ny <= N){
+            if(map[y][x] > map[ny][nx]){
+                dp[y][x] += dfs(nx,ny);
+            }
+        } 
+    }    
+    return dp[y][x];
 }
 
-int main() {
-	cin.tie(NULL);
-	ios::sync_with_stdio(false);
-
-	cin >> m >> n;
-	for (int i = 1; i <= m; i++)
-		for (int j = 1; j <= n; j++)
-			cin >> arr[i][j];
-	
-	
-	cout << dfs(1, 1)<< '\n';
-	return 0;	
+int main(){
+    cin.tie(NULL);
+    ios::sync_with_stdio(false);
+    cin>>N>>M;
+    for(int i=1; i<=N; ++i)
+        for(int j=1; j<=M;++j)
+            cin>>map[i][j];
+    
+    cout<<dfs(1,1)<<endl;
 }
