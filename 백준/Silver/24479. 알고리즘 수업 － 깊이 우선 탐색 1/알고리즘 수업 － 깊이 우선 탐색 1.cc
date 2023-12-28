9fb048c3d19080ade323134dfd2889a4c87bcceb
jiwon <83341978+jiione@users.noreply.github.com>
2023-12-28 19:12:12 +0900

    [Silver II] Title: 알고리즘 수업 - 깊이 우선 탐색 1, Time: 120 ms, Memory: 11756 KB -BaekjoonHub

---
+++ 백준/Silver/24479. 알고리즘 수업 － 깊이 우선 탐색 1/알고리즘 수업 － 깊이 우선 탐색 1.cc
@@ -0,0 +1,68 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+
+#define endl '\n'
+
+using namespace std;
+
+struct node{
+    int n;
+    vector<node*> nexts; 
+}nodes[100001];
+
+int visited[100001] = {0,};
+
+
+
+void init(int N){
+    for(int i=1; i<=N;++i){
+        nodes[i].n=i;
+    }
+}
+
+int cnt=0;
+
+void dfs(int n){
+    for(node* nd : nodes[n].nexts){
+        if(!visited[nd->n]){
+            visited[nd->n]=++cnt;
+            dfs(nd->n);
+            
+        }
+    }
+}
+
+int main(){
+    cin.tie(NULL);
+    ios::sync_with_stdio(false);
+
+    int N,M,R;
+
+    cin>>N>>M>>R;
+    init(N);
+
+    for(int i=0; i<M; ++i){
+        int a,b;
+        cin>>a>>b;
+        nodes[a].nexts.push_back(&nodes[b]);
+        nodes[b].nexts.push_back(&nodes[a]);
+        
+    }
+
+    for(int i=1; i<=N; ++i){
+        sort(nodes[i].nexts.begin(),nodes[i].nexts.end()
+        ,[](node* a, node* b){
+            return a->n<b->n;
+        });
+    }
+
+    visited[R]=++cnt;
+    dfs(R);
+
+    for(int i=1; i<=N; ++i){
+        cout<<visited[i]<<endl;
+    }
+    return 0;
+
+}
