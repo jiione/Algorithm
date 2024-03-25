4d72d6fc15dfe06e7d20bfbbd12fdfafb74b80fb
jiwon <83341978+jiione@users.noreply.github.com>
2024-03-25 16:38:36 +0900

    [Silver V] Title: 돌 게임, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/9655. 돌 게임/돌 게임.cc
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int N;        
+	cin >> N;
+    
+	if (N % 2 == 1) {
+		cout << "SK";
+	}
+
+	else {
+		cout << "CY";
+	}
+
+	return 0;
+}
