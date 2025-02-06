a3432180b553f2dc16d1a171096e97e80d6db5e2
jiwon <83341978+jiione@users.noreply.github.com>
2025-02-06 17:23:10 +0900

    [Silver II] Title: N과 M (12), Time: 212 ms, Memory: 24252 KB -BaekjoonHub

---
+++ 백준/Silver/15666. N과 M （12）/N과 M （12）.java
@@ -0,0 +1,56 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.LinkedHashSet;
+import java.util.Set;
+
+public class Main {
+    static int N;
+    static int M;
+    static int[] nums;
+    static int[] board;
+
+    static Set<String> set = new LinkedHashSet<String>();
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        String[] input = br.readLine().split(" ");
+
+        N = Integer.parseInt(input[0]);
+        M = Integer.parseInt(input[1]);
+        nums = new int[M];
+        board = new int[N];
+
+        input = br.readLine().split(" ");
+        for (int i = 0; i < N; i++) {
+            board[i] = Integer.parseInt(input[i]);
+        }
+
+        Arrays.sort(board);
+
+        for(int i=0; i<N; ++i){
+            nums[0] = board[i];
+            dfs(1,i);
+        }
+
+        set.stream().forEach(System.out::println);
+    }
+
+    static void dfs(int len, int idx){
+        if(len == M){
+            StringBuilder sb = new StringBuilder();
+            for(int i=0; i<M; ++i){
+                sb.append(nums[i]).append(" ");
+            }
+            set.add(sb.toString());
+            return;
+        }
+
+        for(int i=idx; i<N; ++i){
+                nums[len] = board[i];
+                dfs(len+1,i);
+        }
+    }
+}
